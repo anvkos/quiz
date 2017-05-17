3de5c1b937eb8adb8c60a4e6bb71371989907c75
Anton Kosolapov <kosolapov@netpost.ru>
2017-05-17 11:10:18 +0700

    *: added more tests for #place_in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -28,4 +28,35 @@ RSpec.describe User, type: :model do
       expect(user.author?(entity)).to be_falsey
     end
   end
+
+  describe '#place_in' do
+    let(:user) { create(:user) }
+    let(:quiz) { create(:quiz) }
+
+    it 'returns last place in quiz' do
+      5.times do |i|
+        create(:rating, quiz: quiz, max_score: i + 1)
+      end
+      create(:rating, quiz: quiz, user: user, max_score: 0)
+      expect(user.place_in(quiz)).to eq 6
+    end
+
+    it 'returns first place in quiz' do
+      5.times do |i|
+        create(:rating, quiz: quiz, max_score: i + 1)
+      end
+      create(:rating, quiz: quiz, user: user, max_score: 5)
+      expect(user.place_in(quiz)).to eq 1
+    end
+
+    it 'returns place in quiz' do
+      ratings = []
+      5.downto(1) do |i|
+        ratings << create(:rating, quiz: quiz, max_score: i * 10)
+      end
+      place = rand(0..5)
+      create(:rating, quiz: quiz, user: user, max_score: ratings[place].max_score + 1)
+      expect(user.place_in(quiz)).to eq place + 1
+    end
+  end
 end
