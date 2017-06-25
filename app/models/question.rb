2cf57061817c2d9953d800f91c8e168c74f2fbb4
Anton Kosolapov <anvkos@ya.ru>
2017-06-25 18:29:45 +0700

    *: question validate - must be correct answer

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,4 +5,13 @@ class Question < ApplicationRecord
   accepts_nested_attributes_for :answers, reject_if: :all_blank, allow_destroy: true
 
   validates :body, presence: true
+  validate :must_be_correct_answer
+
+  private
+
+  def must_be_correct_answer
+    unless answers.map(&:correct).include?(true)
+      errors.add(:base, 'No answer is correct')
+    end
+  end
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -9,5 +9,31 @@ RSpec.describe Question, type: :model do
 
   describe 'validations' do
     it { should validate_presence_of :body }
+
+    describe '#must_be_correct_answer' do
+      context 'there is no correct answer' do
+        before do
+          @question = build(:question)
+          @question.answers << build(:answer, question: @question, correct: false)
+        end
+
+        it 'validate false' do
+          expect(@question.valid?).to eq false
+        end
+
+        it 'question error - no answer is correct' do
+          @question.valid?
+          expect(@question.errors[:base]).to  include('No answer is correct')
+        end
+      end
+
+      context 'correct answer exists' do
+        it 'validate true' do
+          question = build(:question)
+          question.answers << build(:answer, question: question, correct: true)
+          expect(question.valid?).to eq true
+        end
+      end
+    end
   end
 end
